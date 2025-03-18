311b29e34b1a8cb5d7ae61bcccd9e315a5d0966c
Vaibhav G <vaibhavvishnu456@gmail.com>
2025-03-18 15:08:25 +0530

    prisma project for a contacts db

---
+++ src/index.ts
@@ -0,0 +1,77 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client'
+const app = new Hono()
+const prisma = new PrismaClient()
+// GET /contacts
+app.get("/contacts", async (context) => {
+  try {
+    const contact = await prisma.contact.findMany()
+    return context.json({contact}, 200)
+  }
+  catch {
+    return context.json({error: " Error fetching contacts"}, 404)
+  }
+});
+// POST /contacts
+app.post("/contacts", async (context) => {
+  const { name, phoneNumber, email } = await context.req.json();
+  try {
+    const contact = await prisma.contact.create(
+      {
+        data: { name: name, phoneNumber: phoneNumber, email: email }
+      });
+    return context.json(contact, 201);
+  }
+  catch {
+    return context.json({ error: "404 Not Found: Error creating contact" }, 404);
+  }
+});
+// PATCH /contacts/:id
+app.patch("/contacts/:id", async (context) => {
+  const id = context.req.param("id");
+  const { name, phoneNumber, email } = await context.req.json();
+  try {
+    const con = await prisma.contact.update({
+      where: { id: id },
+      data: { name:name , phoneNumber: phoneNumber, email: email },
+    });
+    return context.json(
+      {
+        contact: con
+      }, 200);
+  }
+  catch {
+    return context.json({ error: "404 Not Found: Error updating contact" }, 404);
+  }
+});
+// DELETE /contacts/:id
+app.delete("/contacts/:id", async (context) => {
+  const id = context.req.param("id");
+  try {
+    const con = await prisma.contact.delete(
+      {
+        where: { id: id }
+      });
+    return context.json({contact: con}, 200)
+  }
+  catch {
+    return context.json({ error: "404 Not Found: Error deleting contact" }, 404);
+  }
+});
+serve(app)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
